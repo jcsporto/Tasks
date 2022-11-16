1cce9a2b4c39af1c2b429b714682e4ed9e22b49c
Jean Porto <portojean@gmail.com>
2022-11-16 09:40:32 -0300

    feat: add test

--- Rspec/Rails/test_like_a_hero/spec/models/user_spec.rb
+++ Rspec/Rails/test_like_a_hero/spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "is invalid if the level is not between 1 and 99"
+  it "returns the corret hero title"
+end
+
+RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+

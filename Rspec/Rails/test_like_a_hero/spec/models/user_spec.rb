6c1a07809d67b0378ddf73ea7d18606952080253
Jean Porto <portojean@gmail.com>
2022-11-16 21:06:56 -0300

    feat: add it

--- Rspec/Rails/test_like_a_hero/spec/models/user_spec.rb
+++ Rspec/Rails/test_like_a_hero/spec/models/user_spec.rb
@@ -4,9 +4,14 @@ RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
     expect(User.create(nickname: 'Chronos', kind: :wizard, level: 100)).to_not be_valid 
   end  
-  it "returns the corret hero title"
+
+  it "returns the corret hero title" do
+    user = User.create(nickname: 'Chronos', kind: :wizard, level: 1)
+    expect(user.title).to eq('wirzard Chronos #1')
+  end
 end
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+

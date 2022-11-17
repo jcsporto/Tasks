e761b7d73f82b37feb53507f2c16294acf9b2ef9
Jean Porto <portojean@gmail.com>
2022-11-17 08:39:16 -0300

    refactor: base de teste

--- Rspec/Rails/test_like_a_hero/spec/requests/users_spec.rb
+++ Rspec/Rails/test_like_a_hero/spec/requests/users_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
-      expect(response).to have_http_status(200)
+    it "returns success status"
+    it "the user's title is present"   
+  end
+
+  describe "POST/users" do
+    context "when it has valid parameters" do
+      it "creates the user with correct attributes"      
     end
+
+    context "when it has no valid parameters" do
+      it "does not create user"
+    end 
   end
 end

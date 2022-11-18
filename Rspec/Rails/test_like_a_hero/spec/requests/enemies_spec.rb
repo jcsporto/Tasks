9ce2490c94f5ab948964cbf6c97f0d0d0ee9b63a
Jean Porto <portojean@gmail.com>
2022-11-18 20:59:37 -0300

    feat: incluindo os testes do update

--- Rspec/Rails/test_like_a_hero/spec/requests/enemies_spec.rb
+++ Rspec/Rails/test_like_a_hero/spec/requests/enemies_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Enemies", type: :request do
   describe "GET /enemies" do
-    it "works! (now write some real specs)" do
-      get enemies_path
-      expect(response).to have_http_status(200)
+    context 'when the enemy exists' do
+      it 'returns status code 200'
+      it 'updates the record'
+      it 'returns the enemy updated'
+    end
+
+    context 'when the enemy does not exists' do
+      it 'returns status code 404'
+      it 'returns a not found message'
     end
   end
 end

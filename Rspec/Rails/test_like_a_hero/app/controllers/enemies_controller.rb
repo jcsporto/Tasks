a303c1f19f1edf09cee853758ef7d99cba923813
Jean Porto <portojean@gmail.com>
2022-11-18 20:38:00 -0300

    feat: add metodos auxiliares no controller

--- Rspec/Rails/test_like_a_hero/app/controllers/enemies_controller.rb
+++ Rspec/Rails/test_like_a_hero/app/controllers/enemies_controller.rb
@@ -4,4 +4,18 @@ class EnemiesController < ApplicationController
 
   def destroy
   end
+
+  before_action :set_enemy
+
+  private 
+  
+  def enemy_params
+    params.permit(:name, :power_base, :power_step, :level, :kind)
+  end
+
+  def set_enemy 
+    @enemy = Enemy.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    rende json: { message: e.message }, status: :not_found 
+  end
 end

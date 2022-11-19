625def93c09e65b20f712ecfb9400033509d5d49
Jean Porto <portojean@gmail.com>
2022-11-18 21:19:27 -0300

    feat: add it update enemy

--- Rspec/Rails/test_like_a_hero/spec/requests/enemies_spec.rb
+++ Rspec/Rails/test_like_a_hero/spec/requests/enemies_spec.rb
@@ -9,8 +9,15 @@ RSpec.describe "Enemies", type: :request do
         put "/enemies/#{enemy.id}", params: enemy_attributes
         expect(response).to have_http_status(200)
       end
-      
-      it 'updates the record'
+
+      it 'updates the enemy'do
+        enemy = create( :enemy)
+        enemy_attributes = attributes_for( :enemy)
+        put "/enemies/#{enemy.id}", params: enemy_attributes
+        
+        expect(enemy.reload).to have_attibutes(enemy_attributes)
+      end
+
       it 'returns the enemy updated'
     end
 

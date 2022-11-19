892c2934c80bd3a8e2c25fd87bc04d6bfc0e6bdc
Jean Porto <portojean@gmail.com>
2022-11-18 21:39:39 -0300

    feat: DELETE /enemies

--- Rspec/Rails/test_like_a_hero/spec/requests/enemies_spec.rb
+++ Rspec/Rails/test_like_a_hero/spec/requests/enemies_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe "Enemies", type: :request do
         expect(response).to have_http_status(200)
       end
 
+      it 'returns status code 204' do
+        enemy = create( :enemy)
+        delete "/enemies/#{enemy.id}"
+        expect(response).to have_http_status(204)
+      end
+
       it 'updates the enemy'do
         enemy = create( :enemy)
         enemy_attributes = attributes_for( :enemy)
@@ -38,6 +44,18 @@ RSpec.describe "Enemies", type: :request do
         put '/enemies/0', params: attributes_for(:enemy)
         expect(response.body).to match(/Couldn't find Enemy/)
       end
+    end
+  end
+
+  describe "DELETE /enemies" do
+    context 'when the enemy exists' do
+        it 'returns status code 200'
+        it 'destroy the record'
+    end
+
+    context 'when the enemy does not exists' do
+      it 'returns status code 404'
+      it 'returns a not found message'
     end
   end
 end

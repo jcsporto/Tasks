3a3b81b2dd5379e6bf663f41f445ab25b9fb0331
Jean Porto <portojean@gmail.com>
2022-11-22 05:48:41 -0300

    add teste

--- Rspec/rspec_rails/test_like_a_hero/spec/models/user_spec.rb
+++ Rspec/rspec_rails/test_like_a_hero/spec/models/user_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+   pending "add some examples to (or delete) #{__FILE__}"  
+   end
+   
+end
+
+RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
     expect(User.create(nickname: 'Chronos', kind: :wizard, level: 100)).to_not be_valid 
   end  
